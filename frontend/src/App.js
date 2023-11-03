98239f4c326651a15487ffe059a876a6f729bc0f
Eoin728 <eoiniecummins@gmail.com>
2023-11-03 12:35:26 +0000

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,25 @@
-import LoginPage from './Pages/LoginPage';
-import SongsPage from './Pages/SongsPage';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-
-
-
 
 function App() {
   return (
-
     <div className="App">
-  
-      <Router>
-  
-    
-      <Routes>
-     
-      <Route path = "/" exact element = { <LoginPage/> } />
-      <Route path = "/songs"  element = {  <SongsPage/>} />
-      </Routes>
-  
-    </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
-/*
-
-*/
