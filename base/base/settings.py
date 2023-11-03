2d6dfffafe1d66147a3eb144f4df5b45d04cf998
Eoin728 <147709127+Eoin728@users.noreply.github.com>
2023-11-03 01:20:30 +0000

    Update settings.py

--- base/base/settings.py
+++ base/base/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
-
+SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
